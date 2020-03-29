9f89c64d26a48390b281f89cf8208a870a7c02b1
Fargui <gui.dumez@gmail.com>
2020-03-29 15:44:27 +0200

    Caclque Walid

--- app-jquery-php/partials/header.php
+++ app-jquery-php/partials/header.php
@@ -1,18 +1,4 @@
 
-<?php require './config/database.php' ?>
 
 
-<!DOCTYPE html>
-<html lang="fr">
 
-<head>
-    <base href="/~guillaumedumez/Ajax/app-jquery-php/">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AJAX Users</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-</head>
-
-<body>
-    <div class="container">
--- app-jquery-php/user.php
+++ app-jquery-php/user.php
@@ -1,24 +1,41 @@
 <?php
+ require './config/database.php';
 
-$users = [
-    [
-        'firstname' => "Guillaume",
-        'lastname' => "Dumez",
-        'tel_number' => "07.89.30.51.61"
-    ],
-    [
-        'firstname' => "Thibault",
-        'lastname' => "Dumez",
-        'tel_number' => "07.62.21.86.71"
-    ],
-];
 
-if (isset($_POST['name']))
-{
-    array_push($users, ['firstname' => $_POST['name']]); /* Reprend le name dans data */
-}
 
-header('content-type: application/json');
-echo json_encode($users);
-
-$query = $db->query('');
+ $firstname = $_POST['firstname'] ?? null;
+ $lastname = $_POST['lastname'] ?? null;
+ 
+ 
+ 
+ $user = [
+ 
+ ];
+ 
+ $i = $db->query('SELECT * FROM user');
+ 
+ foreach($i as $users){
+ 
+     array_push($user, [
+         'lastname' => $users['lastname'],
+         'firstname' => $users['firstname'],
+     ]);
+ };
+ 
+ if (isset($_POST['firstname'], $_POST['lastname']))
+ {
+ 
+     $query = $db->prepare('INSERT INTO user (`firstname`,`lastname`) VALUES (:firstname , :lastname)');
+     $query->bindValue(':firstname', $firstname);
+     $query->bindValue(':lastname', $lastname);
+     
+     $query->execute();
+ 
+     array_push($user, [
+         'lastname' => $lastname,
+         'firstname' => $firstname
+     ] );
+ }
+ 
+ header(('content-type: application/json'));
+ echo json_encode($user);
